7170b7888f3628211527fcf220aa4fab7e4896d5
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-04-30 15:08:06 -0700

    sucessfully retrieved Pexels API photo using japan as query search string

--- main.js
+++ main.js
@@ -1,21 +1,43 @@
-function getFlightInfo() {
-  const params = {
-    method: 'GET',
-  }
+// function getFlightInfo() {
+//   const params = {
+//     method: 'GET',
+//   }
 
-  fetch('http://api.aviationstack.com/v1/flights?access_key=a8f3f007b5b6084882a8e2c359e3e4d5', params)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data)
-    });
-  // $.ajax({
-  //   url: 'http://api.aviationstack.com/v1/flights',
-  //   data: {
-  //     access_key: 'a8f3f007b5b6084882a8e2c359e3e4d5'
-  //   },
-  //   dataType: 'json',
-  //   success: console.log
-  // });
+//   fetch('http://api.aviationstack.com/v1/flights?access_key=a8f3f007b5b6084882a8e2c359e3e4d5', params)
+//     .then(res => res.json())
+//     .then(data => {
+//       console.log(data)
+//     });
+//   $.ajax({
+//     url: 'http://api.aviationstack.com/v1/flights',
+//     data: {
+//       access_key: 'a8f3f007b5b6084882a8e2c359e3e4d5'
+//     },
+//     dataType: 'json',
+//     success: console.log
+//   });
+// }
+
+// getFlightInfo()
+
+
+
+function getPexelPictures(){
+    const params = {
+    method: 'GET',
+      headers: { 'Authorization': '563492ad6f9170000100000199ba9517fba74485b278a4b9796b71c3' },
+    }
+      fetch('https://api.pexels.com/v1/search?query=japan&per_page=15&page=1', params)
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
+        });
 }
 
-getFlightInfo()
+getPexelPictures()
+
+// const init = {
+//   method: 'POST',
+//   headers: { 'Content-Type': 'application/json' },
+//   body: JSON.stringify(newGrade)
+// };
