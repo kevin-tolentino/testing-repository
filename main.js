4b031c8ed6a2138abbc08e324850d94cf70b4e47
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-04-30 21:59:19 -0700

    worked on testing Frank's version of the fetch() method

--- main.js
+++ main.js
@@ -24,35 +24,55 @@
 
 const container = document.getElementById('container')
 
-function createEl(src) {
-  const newEl = document.createElement('div')
-  newEl.classList.add('col-6')
-  const image = document.createElement('img')
-  image.setAttribute('src', src);
-  newEl.appendChild(image)
-  container.appendChild(newEl)
+// function createEl(src) {
+//   const newEl = document.createElement('div')
+//   newEl.classList.add('col-6')
+//   const image = document.createElement('img')
+//   image.setAttribute('src', src);
+//   newEl.appendChild(image)
+//   container.appendChild(newEl)
+// }
+
+// function getPexelPictures(){
+//     const params = {
+//     method: 'GET',
+//       headers: { 'Authorization': '563492ad6f9170000100000199ba9517fba74485b278a4b9796b71c3' },
+//     }
+//       fetch('https://api.pexels.com/v1/search?query=japan&per_page=15&page=1', params)
+//         .then(res => res.json())
+//         .then(data => {
+//           console.log(data)
+//           console.log(data.photos[0].src.portrait)
+//           for (let i = 0; i < data.photos.length; i++) {
+//             createEl(data.photos[i].src.tiny)
+//           }
+//         });
+// }
+
+function getFlightInfo() {
+  const params = {
+    method: 'GET'
+  }
+  fetch('http://api.aviationstack.com/v1/flights?access_key=a8f3f007b5b6084882a8e2c359e3e4d5&flight_number=5282&flight_date=2020-05-05&dep_iata=SFO', params)
+    .then(res => res.json())
+      .then(data => {
+        console.log(data)
+      });
+  // $.ajax({
+  //   url: 'http://api.aviationstack.com/v1/flights',
+  //   data: {
+  //     access_key: 'a8f3f007b5b6084882a8e2c359e3e4d5'
+  //   },
+  //   dataType: 'json',
+  //   success: console.log
+  // });
 }
-
-function getPexelPictures(){
-    const params = {
-    method: 'GET',
-      headers: { 'Authorization': '563492ad6f9170000100000199ba9517fba74485b278a4b9796b71c3' },
-    }
-      fetch('https://api.pexels.com/v1/search?query=japan&per_page=15&page=1', params)
-        .then(res => res.json())
-        .then(data => {
-          console.log(data)
-          console.log(data.photos[0].src.portrait)
-          for (let i = 0; i < data.photos.length; i++) {
-            createEl(data.photos[i].src.tiny)
-          }
-        });
-}
+getFlightInfo()
 
 
 
 
-getPexelPictures()
+// getPexelPictures()
 
 // const init = {
 //   method: 'POST',
